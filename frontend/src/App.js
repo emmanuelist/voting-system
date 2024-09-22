11e730a7476805dcdd035b0bb80386a06e817ee4
emmanuelist <emmanuel.paul75@yahoo.com>
2024-09-22 23:22:00 +0100

    Refactor App component to fetch voting options and results

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { Connect } from '@stacks/connect-react';
 import { StacksTestnet } from '@stacks/network';
-import { callReadOnlyFunction, contractPrincipalCV, stringAsciiCV, makeContractCall, broadcastTransaction } from '@stacks/transactions';
-import { UserSession } from '@stacks/auth';
+import { callReadOnlyFunction, contractPrincipalCV, stringAsciiCV } from '@stacks/transactions';
 import VotingForm from './components/VotingForm';
 import Results from './components/Results';
 
 const App = () => {
   const [votingOptions, setVotingOptions] = useState([]);
   const [results, setResults] = useState({});
-  const [userSession, setUserSession] = useState(new UserSession());
 
   useEffect(() => {
+    const fetchVotingOptions = async () => {
+      const options = await callReadOnlyFunction({
+        contractAddress: 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM',
+        contractName: 'voting',
+        functionName: 'get-voting-options',
+        network: new StacksTestnet(),
+      });
+      setVotingOptions(options.value.list);
+    };
+
     const fetchResults = async () => {
       const newResults = {};
       for (const option of votingOptions) {
@@ -27,33 +35,10 @@ const App = () => {
       setResults(newResults);
     };
 
-    fetchVotingOptions();
-    fetchResults();
+    fetchVotingOptions().then(fetchResults);
   }, [votingOptions]);
 
-  const fetchVotingOptions = async () => {
-    const options = await callReadOnlyFunction({
-      contractAddress: 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM',
-      contractName: 'voting',
-      functionName: 'get-voting-options',
-      network: new StacksTestnet(),
-    });
-    setVotingOptions(options.value.list);
-  };
-
-  const handleVote = async (option) => {
-    const transaction = await makeContractCall({
-      contractAddress: 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM',
-      contractName: 'voting',
-      functionName: 'vote',
-      functionArgs: [stringAsciiCV(option)],
-      senderKey: userSession.loadUserData().appPrivateKey,
-      network: new StacksTestnet(),
-    });
-
-    const result = await broadcastTransaction(transaction, new StacksTestnet());
-    console.log(result);
-
+  const handleVote = (option) => {
     setResults((prevResults) => ({
       ...prevResults,
       [option]: (prevResults[option] || 0) + 1,
@@ -63,20 +48,6 @@ const App = () => {
   return (
     <div>
       <h1>Voting System</h1>
-      <Connect
-        authOptions={{
-          appDetails: {
-            name: 'Voting App',
-            icon: window.location.origin + '/logo.svg',
-          },
-          redirectTo: '/',
-          onFinish: () => {
-            const userSession = new UserSession();
-            setUserSession(userSession);
-          },
-          userSession,
-        }}
-      />
       <VotingForm onVote={handleVote} />
       <Results results={results} />
     </div>
